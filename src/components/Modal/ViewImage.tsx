8d7934b22e933bd782843d5d2d2ebded42cedd22
Luan <luancmadev@gmail.com>
2021-05-29 13:34:14 -0300

    finished

--- src/components/Modal/ViewImage.tsx
+++ src/components/Modal/ViewImage.tsx
@@ -19,5 +19,33 @@ export function ModalViewImage({
   onClose,
   imgUrl,
 }: ModalViewImageProps): JSX.Element {
-  // TODO MODAL WITH IMAGE AND EXTERNAL LINK
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <ModalOverlay />
+      <ModalContent
+        mx="auto"
+        mb="2.5rem"
+        mt="2.5rem"
+        w="auto"
+        h="auto"
+        bg="transparent"
+      >
+        <ModalBody p="0">
+          <Image src={imgUrl} />
+        </ModalBody>
+
+        <ModalFooter h="2rem" bg="pGray.800" borderBottomRadius="6px">
+          <Link
+            href={imgUrl}
+            isExternal
+            fontSize="0.875rem"
+            mr="auto"
+            ml="10px"
+          >
+            Abrir original
+          </Link>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
 }
